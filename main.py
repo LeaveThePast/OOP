a38d3b295a9f0bc6effca57b9f3ec638a0b160eb
LeaveThePast <Ramil.Budagli@yandex.ru>
2023-01-19 14:48:09 +0400

    update

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+
+
+def homeworks_result(students, course):
+    sum_grades = 0
+    sum_grades_numbers = 0
+    for student in students:
+        if isinstance(student, Student) and course in student.courses_in_progress or course in student.finished_courses:
+            for grade in student.grades.values():
+                sum_grades += sum(grade)
+                sum_grades_numbers += 1
+    avg_grade = sum_grades / sum_grades_numbers
+    return avg_grade
+
+
+def lecturers_result(lecturers, course):
+    sum_grades = 0
+    sum_grades_numbers = 0
+    for lecturer in lecturers:
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
+            for grade in lecturer.grades.values():
+                sum_grades += sum(grade)
+                sum_grades_numbers += 1
+    avg_grade = sum_grades / sum_grades_numbers
+    return avg_grade
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -6,6 +32,23 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def __str__(self):
+        return f'Имя: {self.name}\nИмя: {self.surname}\nСредняя оценка за домашние задания" ' \
+               f'{self.calculate_total_grades() / (len(self.courses_in_progress) + len(self.finished_courses))}\n' \
+               f'Курсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}\n'
+
+    def __cmp__(self, other):
+        if not isinstance(other, Student):
+            print('Ошибка')
+            return
+        else:
+            if self.calculate_total_grades() / (len(other.courses_in_progress) + len(other.finished_courses)) > other.calculate_total_grades() / (len(other.courses_in_progress) + len(other.finished_courses)):
+                return f'Средний балл студента {self.name} {self.surname}  больше, чем средний балл студента {other.name} {other.surname}'
+            if self.calculate_total_grades() / (len(other.courses_in_progress) + len(other.finished_courses)) < other.calculate_total_grades() / (len(other.courses_in_progress) + len(other.finished_courses)):
+                return f'Средний балл студента {self.name} {self.surname} меньше, чем средний балл студента {other.name} {other.surname}'
+            if self.calculate_total_grades() / (len(other.courses_in_progress) + len(other.finished_courses)) == other.calculate_total_grades() / (len(other.courses_in_progress) + len(other.finished_courses)):
+                return f'Средний балл студента {self.name} {self.surname} равен среднему баллу студента {other.name} {other.surname}'
 
     def calculate_total_grades(self):
         total_grades = 0
@@ -23,10 +66,7 @@ class Student:
         else:
             return 'Ошибка'
 
-    def __str__(self):
-        return f'Имя: {self.name}\nИмя: {self.surname}\nСредняя оценка за домашние задания" ' \
-               f'{self.calculate_total_grades() / (len(self.courses_in_progress) + len(self.finished_courses))}\n' \
-               f'Курсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}\n'
+
 
 
 class Mentor:
@@ -50,6 +90,18 @@ class Lecturer(Mentor):
     def __str__(self):
         return f'Имя: {self.name}\nИмя: {self.surname}\nСредняя оценка за лекции" ' \
                f'{self.calculate_total_grades() / len(self.courses_attached)}\n '
+
+    def __cmp__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Ошибка')
+            return
+        else:
+            if self.calculate_total_grades() / len(self.courses_attached) > other.calculate_total_grades() / len(other.courses_attached):
+                return f'Средний балл лектора {self.name} {self.surname}  больше, чем средний балл лектора {other.name} {other.surname}'
+            if self.calculate_total_grades() / len(self.courses_attached) < other.calculate_total_grades() / len(other.courses_attached):
+                return f'Средний балл лектора {self.name} {self.surname} меньше, чем средний балл лектора {other.name} {other.surname}'
+            if self.calculate_total_grades() / len(self.courses_attached) == other.calculate_total_grades() / len(other.courses_attached):
+                return f'Средний балл лектора {self.name} {self.surname} равен среднему баллу лектора {other.name} {other.surname}'
 
 
 class Reviewer(Mentor):
@@ -94,7 +146,6 @@ student_1.rate_lecturer(lecturer_1, 'French', 8)
 student_2.rate_lecturer(lecturer_2, 'Python', 9)
 student_2.rate_lecturer(lecturer_2, 'English', 9)
 
-
 print('\nОценки студентов:')
 print(student_1.grades)
 print(student_2.grades)
@@ -115,31 +166,10 @@ print('\nСтуденты:')
 print(student_1)
 print(student_2)
 
-def homeworks_result(students, course):
-    sum_grades = 0
-    sum_grades_numbers = 0
-    for student in students:
-        if isinstance(student, Student) and course in student.courses_in_progress or course in student.finished_courses:
-            for grade in student.grades.values():
-                sum_grades += sum(grade)
-                sum_grades_numbers += 1
-    avg_grade = sum_grades / sum_grades_numbers
-    return avg_grade
-
-
-def lecturers_result(lecturers, course):
-    sum_grades = 0
-    sum_grades_numbers = 0
-    for lecturer in lecturers:
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
-            for grade in lecturer.grades.values():
-                sum_grades += sum(grade)
-                sum_grades_numbers += 1
-    avg_grade = sum_grades / sum_grades_numbers
-    return avg_grade
-
-
 print('Cредняя оценка за домашние задания по студентам в рамках конкретного курса: ', homeworks_result([student_1,
                                                                                                         student_2],
                                                                                                        'Python'))
 print('Средняя оценка за лекции лекторов в рамках курса', lecturers_result([lecturer_1, lecturer_2], 'Python'))
+
+print(lecturer_1.__cmp__(lecturer_2))
+print(student_1.__cmp__(student_2))
